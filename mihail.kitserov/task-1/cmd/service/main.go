4e42e4ea37640d24c934305368d8b15147ae5249
Kreniker <kitserov.mike@gmail.com>
2026-09-11 12:26:28 +0300

    [TASK-1] Scan and check variables

--- mihail.kitserov/task-1/cmd/service/main.go
+++ mihail.kitserov/task-1/cmd/service/main.go
@@ -1,5 +1,26 @@
 package main
 
+import (
+	"fmt"
+)
+
 func main() {
-	
+	var first_argument int
+	_, err := fmt.Scan(&first_argument)
+	if err != nil {
+		fmt.Println("Invalid first operand")
+		return
+	}
+	var second_argument int
+	_, err = fmt.Scan(&second_argument)
+	if err != nil {
+		fmt.Println("Invalid second operand")
+		return
+	}
+	var operation string
+	_, err = fmt.Scan(&operation)
+	if err != nil {
+		fmt.Println("Invalid operation")
+		return
+	}
 }
